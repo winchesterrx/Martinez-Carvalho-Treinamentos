45e65255f5a143e0d6a2db1f9cf215a05406fd49
winchesterrx <123671820+winchesterrx@users.noreply.github.com>
2024-12-23 16:30:20 -0300

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,18 +1,18 @@
 <?php
 // Verifica se as constantes já estão definidas, se não estiverem, define-as.
 if (!defined('SERVIDOR')) {
-    define('SERVIDOR', 'localhost'); // ou seu servidor de banco de dados
+    define('SERVIDOR', 'martinezvideo.mysql.uhserver.com'); // ou seu servidor de banco de dados
 }
 
 if (!defined('USUARIO')) {
-    define('USUARIO', 'root'); // ou seu usuário de banco de dados
+    define('USUARIO', 'winchester123'); // ou seu usuário de banco de dados
 }
 
 if (!defined('SENHA')) {
-    define('SENHA', ''); // ou sua senha de banco de dados
+    define('SENHA', '@Saopaulop45'); // ou sua senha de banco de dados
 }
 
 if (!defined('BANCO')) {
-    define('BANCO', 'treinamentos'); // ou seu banco de dados
+    define('BANCO', 'martinezvideo'); // ou seu banco de dados
 }
 ?>
